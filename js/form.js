111a4386f61b1a5ff89c83fba48a61564b5ac507
dmitrydanilovich10121969 <103949996+dmitrydanilovich10121969@users.noreply.github.com>
2022-06-30 16:22:41 +0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,36 +1,53 @@
 (() => {
-function setBurger(params) {
-  const btn = document.querySelector(`.${params.btnClass}`);
-  const menu = document.querySelector(`.${params.menuClass}`);
 
-  menu.addEventListener("animationend", function () {
-    if (this.classList.contains(params.hiddenClass)) {
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+
+  search.addEventListener("animationend", function (evt) {
+    if (this._isOpened) {
       this.classList.remove(params.activeClass);
       this.classList.remove(params.hiddenClass);
+      this._isOpened = false;
+    } else {
+      this._isOpened = true;
     }
   });
 
-  btn.addEventListener("click", function () {
-    this.classList.toggle(params.activeClass);
+  search.addEventListener('click', function(evt) {
+    evt._isSearch = true;
+  });
+
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
 
     if (
-      !menu.classList.contains(params.activeClass) &&
-      !menu.classList.contains(params.hiddenClass)
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
     ) {
-      menu.classList.add(params.activeClass);
-      document.body.style.overflow = 'hidden';
-    } else {
-      menu.classList.add(params.hiddenClass);
-      document.body.removeAttribute('style');
+      search.classList.add(params.activeClass);
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+
+  document.body.addEventListener('click', function (evt) {
+    if (!evt._isSearch && search._isOpened) {
+      openBtn.disabled = false;
+      search.classList.add(params.hiddenClass);
     }
   });
 }
 
-// здесь мы вызываем функцию и передаем в нее классы наших элементов
-setBurger({
-  btnClass: "js-burger", // класс бургера
-  menuClass: "js-menu-wrap", // класс меню
-  activeClass: "is-active", // класс открытого состояния
+setSearch({
+  openBtnClass: "js-open-search", // класс кнопки открытия
+  closeBtnClass: "js-close-search", // класс кнопки закрытия
+  searchClass: "js-form", // класс формы поиска
+  activeClass: "is-opened", // класс открытого состояния
   hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
 });
 })() ;
